ceca292e9dbb852600c51477ce220f5c7aa79056
Hồ Văn Hoàng <hovanhoang959@gmail.com>
2026-09-06 23:05:48 +0700

    BaiTapVongLap_Bai3

---
+++ BaiTapVongLap/HoVanHoang_Vonglap_Bai3.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>)
+{
+    println("Ho va ten: Ho Van Hoang\nMSV: 2415053122319")
+    print("Cac so chan tu 1 den 20: ")
+    for (i in 1..20)
+        if (i % 2 == 0)
+            print("$i ")
+}
