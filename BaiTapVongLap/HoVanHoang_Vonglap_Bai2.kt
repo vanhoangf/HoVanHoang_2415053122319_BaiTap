76e05b8490b1a19606af80d1ee5121c4f4dcbd30
Hồ Văn Hoàng <hovanhoang959@gmail.com>
2026-09-06 23:03:36 +0700

    BaiTapVongLap_Bai2

---
+++ BaiTapVongLap/HoVanHoang_Vonglap_Bai2.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>)
+{
+    println("Ho va ten: Ho Van Hoang\nMSV: 2415053122319")
+    var s:Int = 0
+    for (i in 1..100)
+        s += i
+    print("Tong cac so tu 1 den 100: $s")
+}
